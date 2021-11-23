340afd9aa736c8d9851fce12649cffd7e5a01121
Taneli Härkönen <taneliharkonen@hotmail.com>
2021-11-23 16:29:54 +0200

    Include data folder - Include data folder in the project so sqlite3 will find it - update user_service.py file

--- coWallet/src/services/user_service.py
+++ coWallet/src/services/user_service.py
@@ -3,4 +3,7 @@ from repositories.user_repository import User_Repository
 class User_Service:
 
     def __init__(self):
+        self.user = None
+    
+    def login(self, username:str, password:str) -> bool:
         pass
